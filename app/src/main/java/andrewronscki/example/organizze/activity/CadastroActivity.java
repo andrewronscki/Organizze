65f513b7552349a9e3b7544a1d622689ebed675f
andrewronscki <andrewronscki@gmail.com>
2019-05-15 20:19:13 -0300

    Implementação do Sign In do aplicativo.

--- app/src/main/java/andrewronscki/example/organizze/activity/CadastroActivity.java
+++ app/src/main/java/andrewronscki/example/organizze/activity/CadastroActivity.java
@@ -12,6 +12,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 import andrewronscki.example.organizze.R;
 import andrewronscki.example.organizze.config.ConfiguracaoFirebase;
@@ -82,8 +85,21 @@ public class CadastroActivity extends AppCompatActivity {
                            "Sucesso ao cadastrar usuário!",
                            Toast.LENGTH_SHORT).show();
                }else{
+                   String excecao = "";
+                   try {
+                       throw  task.getException();
+                   } catch(FirebaseAuthWeakPasswordException e) {
+                       excecao = "Digite uma senha mais forte!";
+                   } catch(FirebaseAuthInvalidCredentialsException e) {
+                       excecao = "Por favor, digite um e-mail válido";
+                   } catch(FirebaseAuthUserCollisionException e) {
+                       excecao = "Esta conta já foi cadastrada";
+                   } catch(Exception e) {
+                       excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+                       e.printStackTrace();
+                   }
                    Toast.makeText(CadastroActivity.this,
-                           "Erro ao cadastrar usuário!",
+                           excecao,
                            Toast.LENGTH_SHORT).show();
                }
             }
--- app/src/main/java/andrewronscki/example/organizze/activity/LoginActivity.java
+++ app/src/main/java/andrewronscki/example/organizze/activity/LoginActivity.java
@@ -1,15 +1,100 @@
 package andrewronscki.example.organizze.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 import andrewronscki.example.organizze.R;
+import andrewronscki.example.organizze.config.ConfiguracaoFirebase;
+import andrewronscki.example.organizze.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText campoEmail, campoSenha;
+    private Button botaoEntrar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        campoEmail = findViewById(R.id.editEmail);
+        campoSenha = findViewById(R.id.editSenha);
+        botaoEntrar = findViewById(R.id.buttonEntrar);
+
+        botaoEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+                if(!textoEmail.isEmpty()) {
+                    if(!textoSenha.isEmpty()) {
+                        usuario = new Usuario();
+                        usuario.setEmail(textoEmail);
+                        usuario.setSenha(textoSenha);
+                        validarLogin();
+
+                    } else{
+                        Toast.makeText(LoginActivity.this,
+                                "Preencha a senha!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else{
+                    Toast.makeText(LoginActivity.this,
+                            "Preencha o email!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void validarLogin() {
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this,
+                            "Sucesso ao fazer login",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    String excecao = "";
+                    try {
+                        throw  task.getException();
+                    } catch(FirebaseAuthInvalidUserException e) {
+                        excecao = "Usuário não está cadastrado!";
+                    } catch(FirebaseAuthInvalidCredentialsException e) {
+                        excecao = "E-mail ou senha não correspondem a um usuário cadastrado";
+                    } catch(Exception e) {
+                        excecao = "Erro ao logar com o usuário inserido: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this,
+                            excecao,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
