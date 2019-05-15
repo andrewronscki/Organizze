2035747417e0109625a547d8c1e797c1f87b00ae
andrewronscki <andrewronscki@gmail.com>
2019-05-15 07:22:28 -0300

    Primeiro commit

---
+++ app/src/main/java/andrewronscki/example/organizze/activity/CadastroActivity.java
@@ -0,0 +1,15 @@
+package andrewronscki.example.organizze.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import andrewronscki.example.organizze.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
